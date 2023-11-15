9b3bc680116657b213f2094500c665605c81756e
jijie21 <jennyjijie21@gmail.com>
2023-11-14 17:26:20 -0800

    add single mode

--- cli.py
+++ cli.py
@@ -1,35 +1,52 @@
 from logic import check_winner, get_empty_board, print_board, get_player_input, switch_player
+from bot import Bot
 import logging
-# add the log file record the games 
-logging.basicConfig(
-    filename='log/msg.log',
-    level=logging.INFO
-)
-         
 
-if __name__ == '__main__': #program excute directly
-    current_player = "X"
-    board = get_empty_board()
-    winner = None 
-    
-    while winner is None: 
-        print_board(board)
-        try:
-           row, col = get_player_input(current_player)
-        
-        except ValueError:
-            print("Invaild, try again")
-            continue
-        except IndexError:
-            print("Invaild, try again")
-            continue
 
-        print(row)
-        print(col)
+class TicTacToeGame:
+    def __init__(self, mode):
+        self.current_player = "X"
+        self.board = get_empty_board()
+        self.winner = None
+        self.mode = mode
+        self.bot = Bot()
 
-        board[row][col] = current_player
-        winner = check_winner(board)
-        current_player = switch_player(current_player)
-print_board(board)
-print(f"winner is {winner}")
-logging.info(f'Player {current_player} made a move')
+    def play_game(self):
+        while self.winner is None:
+            print_board(self.board)
+            
+            if self.mode == "single" and self.current_player == "O":
+                row, col = self.bot.get_bot_move(self.board)
+            else:
+                try:
+                    row, col = get_player_input(self.current_player)
+                except ValueError:
+                    print("Invalid input, try again")
+                    continue
+                except IndexError:
+                    print("Invalid input, try again")
+                    continue
+
+            self.board[row][col] = self.current_player
+            self.winner = check_winner(self.board)
+            self.current_player = switch_player(self.current_player)
+
+        print_board(self.board)
+        print(f"Winner is {self.winner}")
+        logging.info(f'Player {self.current_player} made a move')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename='log/msg.log',
+        level=logging.INFO
+    )
+
+    game_mode = input("Enter game mode (single or two): ").lower()
+
+    if game_mode not in ["single", "two"]:
+        print("Invalid game mode. Exiting.")
+        exit()
+
+    game = TicTacToeGame(game_mode)
+    game.play_game()
